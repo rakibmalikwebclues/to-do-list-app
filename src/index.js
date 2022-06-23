ba6d6dc2848e6d28e32b3936516ac548253218a9
rakibmalik <rakib@webcluesinfotech.com>
2022-06-23 15:21:53 +0530

    Removed strict mode

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './index.css';
 import App from './App';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
+
+// ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>
+//   </React.StrictMode>,
+//   document.getElementById('root')
 // );
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
